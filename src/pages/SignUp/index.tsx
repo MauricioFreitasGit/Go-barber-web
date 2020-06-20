c8d16d6b8accfe57589b8df792d4c94b52b62f86
Mauricio <mauricio@MacBook-Pro-de-User.local>
2020-06-20 20:30:15 -0300

    funçoes de usabilidade + tela de cadastro

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+
+import { Form } from '@unform/web';
+import { Container, Content, Background } from './styles';
+import logoimg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <Background />
+
+      <Content>
+        <img src={logoimg} alt="GoBerber" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+          <Input icon={FiUser} name="name" placeholder="Nome" />
+          <Input icon={FiMail} name="email" placeholder="E-mail" />
+          <Input
+            icon={FiLock}
+            name="password"
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para login
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
